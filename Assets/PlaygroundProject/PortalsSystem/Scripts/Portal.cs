bd3ceb3595e5930401a6501a3b891fce6653e257
ChristianBeninca <christianbeninca@outlook.com>
2022-11-19 17:22:43 -0400

    Portal finally working
    Signed-off-by: ChristianBeninca <christianbeninca@outlook.com>

--- Assets/PlaygroundProject/PortalsSystem/Scripts/Portal.cs
+++ Assets/PlaygroundProject/PortalsSystem/Scripts/Portal.cs
@@ -7,14 +7,18 @@ using UnityEngine.Events;
 
 public class Portal : MonoBehaviour
 {
-    bool portalLoaded = false;
+    bool
+        portalLoaded = false,
+        playerIsOverlapping = false;
     public Transform playerCamera, otherPortal;
     Camera otherCamera;
     Material thisMaterial;
+    GameObject player;
     [SerializeField] string inputKey, outputKey;
         public string InputKey { get => inputKey; }
         public string OutputKey { get => outputKey; set => outputKey = value; }
     [SerializeField] Shader portalShader;
+    [SerializeField] BoxCollider portalCollider;
 
     void Start()
     {
@@ -22,10 +26,76 @@ public class Portal : MonoBehaviour
         Setup();
     }
 
+    void Update()
+    {
+        if (portalLoaded)
+            SyncCameraTransform();
+
+        if (playerIsOverlapping)
+        {
+            Teleporter();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            #region Código antigo pra substituir o sistema do brackeys
+            //Debug.Log("<color=green>chegou</color>");
+
+            //player.transform.position = new Vector3(0, 5, 0);
+
+
+            //float rotationDiff = -Quaternion.Angle(transform.rotation, otherPortal.rotation);
+            //rotationDiff += 180;
+            //player.transform.Rotate(Vector3.up, rotationDiff);
+
+            //Vector3 portalToPlayer = player.transform.position - transform.position;
+            //Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+            //player.transform.position = otherPortal.position + positionOffset;
+            #endregion
+
+            playerIsOverlapping = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            playerIsOverlapping = false;
+        }
+    }
+
+    void Teleporter()
+    {
+        Vector3 portalToPlayer = player.transform.position - (transform.position + portalCollider.center);
+        Debug.Log("<color=green>portal to player: " + portalToPlayer + "</color>");
+        float dotProduct = Vector3.Dot(transform.forward, portalToPlayer);
+
+        Debug.Log("<color=green>dotproduct: " + dotProduct + "</color>");
+
+        if (dotProduct > 0f)
+        {
+            Debug.Log("<color=purple>teleport</color>");
+            float rotationDiff = -Quaternion.Angle(transform.rotation, otherPortal.rotation);
+            rotationDiff += 180;
+            player.transform.Rotate(Vector3.up, rotationDiff);
+
+            Vector3 positionOffset = Quaternion.Euler(0f, rotationDiff, 0f) * portalToPlayer;
+            player.transform.position = otherPortal.position;
+
+            //playerIsOverlapping = false;
+        }
+    }
+
     void Setup()
     {
         playerCamera = Camera.main.transform;
+        player = GameObject.FindGameObjectWithTag("Player");
         thisMaterial = GetComponentInChildren<Renderer>().material = new Material(portalShader);
+        portalCollider = GetComponent<BoxCollider>();
 
         int sceneId = PortalLinks.Instance.GetSceneByKey(outputKey);
         Debug.Log("sceneid: " + sceneId);
@@ -63,12 +133,6 @@ public class Portal : MonoBehaviour
         Quaternion portalRotationDifference = Quaternion.AngleAxis(angularDifferenceBetweenPortalRotations + 180, Vector3.up);
         Vector3 newCameraDirection = portalRotationDifference * playerCamera.forward;
         otherCamera.transform.rotation = Quaternion.LookRotation(newCameraDirection, Vector3.up);
-    }
-
-    void Update()
-    {
-        if(portalLoaded)
-            SyncCameraTransform();
     }
 
     Transform GetOtherPortalByKey(string outputKey)
