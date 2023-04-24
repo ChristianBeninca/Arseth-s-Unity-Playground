0dc44ac225e96a14f971450970702ef2b1c336aa
ChristianBeninca <christianbeninca@outlook.com>
2023-04-23 20:32:32 -0400

    Added weapon sway system
    Signed-off-by: ChristianBeninca <christianbeninca@outlook.com>

--- Assets/PlaygroundProject/WeaponsSystem/Scripts/Weapon.cs
+++ Assets/PlaygroundProject/WeaponsSystem/Scripts/Weapon.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    [SerializeField] private Animator anim;
+    //[SerializeField] private Animator anim;
     [SerializeField] private GameObject visuals;
     public float damage;
     public float rateOfFire;
@@ -12,18 +12,34 @@ public class Weapon : MonoBehaviour
     public float ammoCapacity;
     public float magazineSize;
 
+    //[SerializeField] private bool activeSway;
+    [SerializeField] private float swayIntensity;
+    [SerializeField] private float swaySmooth;
+    private Quaternion originRotation;
+
     private bool onSheath = true;
+
+    public virtual void Start()
+    {
+        StartSway();
+    }
+
+    public virtual void Update()
+    {
+        UpdateSway();
+    }
 
     public void Shoot()
     {
-        anim.SetTrigger("Shoot");
+        //anim.SetTrigger("Shoot");
     }
 
     public void Sheath()
     {
         if (onSheath) return;
 
-        anim.SetTrigger("Sheath");
+        //anim.SetTrigger("Sheath");
+        Hide();
         onSheath = true;
     }
 
@@ -31,11 +47,32 @@ public class Weapon : MonoBehaviour
     {
         if (!onSheath) return;
 
-        anim.SetTrigger("Draw");
+        //anim.SetTrigger("Draw");
+        Show();
         onSheath = false;
     }
 
     public void Hide() { visuals.SetActive(false); }
 
     public void Show() { visuals.SetActive(true); }
+
+    #region Sway
+    void StartSway()
+    {
+        originRotation = transform.localRotation;
+    }
+
+    void UpdateSway()
+    {
+        float MouseX = Input.GetAxis("Mouse X");
+        float MouseY = Input.GetAxis("Mouse Y");
+
+        Quaternion swayAdjustmentX = Quaternion.AngleAxis(swayIntensity * -MouseX, Vector3.up);
+        Quaternion swayAdjustmentY = Quaternion.AngleAxis(swayIntensity * MouseY, Vector3.right);
+        Quaternion targetRotation = originRotation * swayAdjustmentX * swayAdjustmentY;
+
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, Time.deltaTime * swaySmooth);
+    }
+
+    #endregion
 }
